c8164aa2c1e99baaad30603aa3d770069e633fe8
Pojan <86289506+xXpoyanXx@users.noreply.github.com>
2024-02-19 09:14:58 +0100

    index.js update

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
-const express = require("express")
-const env =  require("dotenv")
-const mongoose  =  require("mongoose")
-const cors =  require("cors")
-const app = express()
-env.config()
-app.use(express.static('public'))
-app.use(express.json())
+const express = require("express");
+const env = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet"); // Neu hinzugefügt
+const app = express();
+env.config();
+
+app.use(express.static('public'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
-//import routes
-const auth  = require("./routes/auth")
-const category = require("./routes/categories")
-const recmnd = require("./routes/recommended")
-const jobs = require("./routes/Jobs")
-const reviews = require("./routes/review")
+app.use(cors());
 
+// Importiere routes
+const auth = require("./routes/auth");
+const category = require("./routes/categories");
+const recmnd = require("./routes/recommended");
+const jobs = require("./routes/Jobs");
+const reviews = require("./routes/review");
 
-//define routes
-app.use('/api/v1/auth',auth)
-app.use('/api/v1/category',category)
-app.use('/api/v1/recommend',recmnd)
-app.use('/api/v1/jobs',jobs)
-app.use('/api/v1/reviews',reviews)
+// Definiere routes
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/category', category);
+app.use('/api/v1/recommend', recmnd);
+app.use('/api/v1/jobs', jobs);
+app.use('/api/v1/reviews', reviews);
 
 app.get("/", (req, res) =>{
-  res.status(200).send("Hello Backend")
-})
+  res.status(200).send("Hello Mobile-Backend");
+});
 
-//connect mongodb
+// Verwende Helmet, um die Cross-Origin-Opener-Policy einzustellen
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      crossOriginOpenerPolicy: 'same-origin'
+    }
+  }
+}));
+
+// Verbinde mit MongoDB
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
-}).then(console.log("Connected to Database")).catch((err)=> console.warn(err))
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(console.log("Connected to Database")).catch((err) => console.warn(err));
 
-app.listen(process.env.PORT,()=>{
-    console.log("Server started")
-})
+app.listen(process.env.PORT, () => {
+  console.log("Server started");
+});
